a3c13c19455b2d48e891fe042f6137f04c045ccf
n4juzinha <julinha.10733@gmail.com>
2023-12-13 09:22:55 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Album albumDoPTV = new Album();
+albumDoPTV.Nome = "Collide with the sky";
+
+Musica musica1 = new Musica();
+musica1.Nome = "May these noises strartle you in your sleeping tonight";
+musica1.Duracao = 120;
+
+Musica musica2 = new Musica();
+musica2.Nome = "Hell Above";
+musica2.Duracao = 240;
+
+
+
+albumDoPTV.AdicionarMusica(musica1);
+albumDoPTV.AdicionarMusica(musica2);
+
+albumDoPTV.ExibirMusicasDoalbum();
